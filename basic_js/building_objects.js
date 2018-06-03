df52f8598f58e11e9b3fb7864b7c699a09cca00c
Uthaeus <romanlavery@gmail.com>
2018-06-03 11:57:38 -0600

    Objects are useful for storing data in a structured way, and can represent real world objects

---
+++ basic_js/building_objects.js
@@ -0,0 +1,17 @@
+// You may have heard the term object before.
+
+// Objects are similar to arrays, except that instead of using indexes to access and modify their data, you access the data in objects through what are called properties.
+
+// Objects are useful for storing data in a structured way, and can represent real world objects, like a cat.
+
+
+
+var myDog = {
+  
+  'name': 'Skippy',
+  'legs': 4,
+  'tails': 2,
+  'friends': ['Jim', 'Scamp', 'Tess']
+  
+  
+};
