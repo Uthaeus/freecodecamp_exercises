1198a87a88cf93a619d33098831a41d701469d3b
Uthaeus <romanlavery@gmail.com>
2018-08-09 10:53:33 -0600

    Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes

--- int_algorithms/spinal_tap.js
+++ int_algorithms/spinal_tap.js
@@ -4,10 +4,19 @@
 function spinalCase(str) {
   // "It's such a fine line between stupid, and clever."
   // --David St. Hubbins
-  let strArr = str.split(/[\s_-]?=([A-Z])?/);
+  let strArr = str.replace(/[_-]/g, ' ').split(/(?=[A-Z\s])/).join(' ').replace(/  +/g, ' ').split(' ');
+  let result = [];
 
-  return strArr;
+  for (let word in strArr) {
+    result.push(strArr[word].toLowerCase());
+  }
+
+  return result.join('-');
 }
 
 console.log(spinalCase('This Is Spinal Tap'));
 console.log(spinalCase("AllThe-small Things"));
+console.log(spinalCase("Teletubbies say Eh-oh")); // should return "teletubbies-say-eh-oh")
+
+
+// split(/(?=[A-Z_\s])/)
