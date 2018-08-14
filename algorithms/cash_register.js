98dbde02b8e60ce37d03d2a96812548d3bc17034
Uthaeus <romanlavery@gmail.com>
2018-08-13 19:52:44 -0600

    Working on cash register objects

--- algorithms/cash_register.js
+++ algorithms/cash_register.js
@@ -13,9 +13,50 @@
 
 
 function checkCashRegister(price, cash, cid) {
-  var change;
+  var change = cash - price;
   // Here is your change, ma'am.
-  return change;
+  var totalID = cid.reduce(function(accum, next) {
+    return accum + next[1];
+  }, 0.0);
+  totalID = Math.round(totalID * 100) / 100;
+  var hasChange = true;
+  var result = {
+    status: undefined,
+    change: []
+  };
+  var pre = [];
+  let monies = {
+    "PENNY": 0.01,
+    "NICKEL": 0.05,
+    "DIME": 0.10,
+    "QUARTER": 0.25,
+    "ONE": 1,
+    "FIVE": 5,
+    "TEN": 10,
+    "TWENTY": 20,
+    "ONE HUNDRED": 100
+  }
+
+  let eachAmt = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100].reverse();
+  let eachName = ['PENNY', 'NICKEL', 'DIME', 'QUARTER', 'ONE', 'FIVE', 'TEN', 'TWENTY', 'ONE HUNDRED'].reverse();
+
+  while (hasChange == true) {
+    for (var i = 0; i < eachAmt.length; i++) {
+      if (change % eachAmt[i] === 0) {
+        pre.push([eachName[i], eachAmt[i]]);
+        change -= eachAmt[i];
+      }
+      if (change == 0) {
+        result.status = 'OPEN';
+        hasChange = false;
+        break;
+      }
+    }
+  }
+
+
+  return currencyCount;
+
 }
 
 // Example cash-in-drawer array:
@@ -31,3 +72,25 @@ function checkCashRegister(price, cash, cid) {
 
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
 // should return {status: "OPEN", change: [["QUARTER", 0.5]]}
+
+// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // should return {status: "INSUFFICIENT_FUNDS", change: []})
+
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
