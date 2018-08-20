f018c6548e28be5dcad30008d502c760ebf7554a
Uthaeus <romanlavery@gmail.com>
2018-08-20 14:13:35 -0600

    Successfully completed cash register project

--- algorithms/cash_register.js
+++ algorithms/cash_register.js
@@ -14,69 +14,73 @@
 
 function checkCashRegister(price, cash, cid) {
   var change = cash - price;
-  // Here is your change, ma'am.
-  var totalID = cid.reduce(function(accum, next) {
-    return accum + next[1];
-  }, 0.0);
-  totalID = Math.round(totalID * 100) / 100;
-  var hasChange = true;
-  var result = {
-    status: undefined,
-    change: []
-  };
-  var pre = [];
-  let monies = {
-    "PENNY": 0.01,
-    "NICKEL": 0.05,
-    "DIME": 0.10,
-    "QUARTER": 0.25,
-    "ONE": 1,
-    "FIVE": 5,
-    "TEN": 10,
-    "TWENTY": 20,
-    "ONE HUNDRED": 100
+
+  let totalCID = Math.round(cid.reduce((a, b) => {
+    return a + b[1];
+  }, 0.0) * 100) / 100;
+
+  let result = {
+    'status': undefined,
+    'change': []
   }
 
-  let eachAmt = [0.01, 0.05, 0.10, 0.25, 1, 5, 10, 20, 100].reverse();
-  let eachName = ['PENNY', 'NICKEL', 'DIME', 'QUARTER', 'ONE', 'FIVE', 'TEN', 'TWENTY', 'ONE HUNDRED'].reverse();
+  if (change > totalCID) {
+    result['status'] = 'INSUFFICIENT_FUNDS';
+    return result;
+  } else if (change === totalCID) {
+    result['status'] = 'CLOSED';
+    result['change'] = cid;
+    return result;
+  } else {
+    result['status'] = 'OPEN';
+  }
 
-  while (hasChange == true) {
-    for (var i = 0; i < eachAmt.length; i++) {
-      if (change % eachAmt[i] === 0) {
-        pre.push([eachName[i], eachAmt[i]]);
-        change -= eachAmt[i];
-      }
-      if (change == 0) {
-        result.status = 'OPEN';
-        hasChange = false;
-        break;
-      }
+  let pre = [];
+  let amounts = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  let denoms = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
+  let cidAmounts = [];
+
+  for (let pair in cid) {
+    cidAmounts.unshift(cid[pair][1]);
+  }
+
+  for (var i = 0; i < denoms.length; i++) {
+    let tempAmount = 0;
+
+    while (change - amounts[i] >= 0 && cidAmounts[i] > 0) {
+      cidAmounts[i] -= amounts[i];
+      tempAmount += amounts[i];
+      change = Math.round((change - amounts[i]) * 100) / 100;
+    }
+    if (tempAmount > 0) {
+      pre.push([denoms[i], tempAmount]);
     }
   }
 
+  if (change > 0) {
+    result['status'] = 'INSUFFICIENT_FUNDS';
+    return result;
+  }
+  result['change'] = pre;
 
-  return currencyCount;
-
+  return result;
 }
 
-// Example cash-in-drawer array:
-// [["PENNY", 1.01],
-// ["NICKEL", 2.05],
-// ["DIME", 3.1],
-// ["QUARTER", 4.25],
-// ["ONE", 90],
-// ["FIVE", 55],
-// ["TEN", 20],
-// ["TWENTY", 60],
-// ["ONE HUNDRED", 100]]
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-// should return {status: "OPEN", change: [["QUARTER", 0.5]]}
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+// {status: "OPEN", change: [["QUARTER", 0.5]]}
 
-// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // should return {status: "INSUFFICIENT_FUNDS", change: []})
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+// should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]})
 
-//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.)
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+// should return {status: "INSUFFICIENT_FUNDS", change: []})
 
+//console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); 
+// should return {status: "INSUFFICIENT_FUNDS", change: []})
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+// should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]})
 
 
 
