bfde2b7ac7663e729cec0f2b0b29b75997cff1b7
Uthaeus <romanlavery@gmail.com>
2018-12-06 10:33:55 -0700

    fcc tests passing

--- projects/markdown/src/App.js
+++ projects/markdown/src/App.js
@@ -9,7 +9,7 @@ const marked = require("marked");
 class App extends Component {
 
   state = {
-    markdown: ''
+    markdown: "# Header \n## SubHeader\n**Some Bold Text**\n> Here is a blockquote\n[some link](https://www.example.com)\n![alt text](image.jpg)\n`something = () => 'this';`\n```\n{'firstName': 'John',\n'lastName': 'Smith',\n'age': 25}```\n1. First item\n2. Second item\n3. Third item"
   }
 
   updateMarkdown = (markdown) => {
