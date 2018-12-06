4d54ed98a5410ab6ba35ce44171d4719c8b894fe
Uthaeus <romanlavery@gmail.com>
2018-12-06 09:16:56 -0700

    created markdown textarea and update function

--- projects/markdown/src/App.js
+++ projects/markdown/src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+
+const marked = require("marked");
 
 class App extends Component {
+
+  state = {
+    markdown: ''
+  }
+
+  updateMarkdown = (markdown) => {
+    this.setState({
+      markdown  
+    })
+  }
+
   render() {
+    let markdown = this.state;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <div>
+          <FormGroup controlId="formControlsTextarea">
+            <ControlLabel>Markdown Input</ControlLabel>
+            <FormControl componentClass="textarea" placeholder="Enter Markdown" value={markdown} onChange={(event) => this.updateMarkdown(event.target.value)}></FormControl>
+          </FormGroup>
+        </div>
+        <div>
+          <h1>Markdown Output</h1>
+          <div>
+
+          </div>
+        </div>
       </div>
     );
   }
