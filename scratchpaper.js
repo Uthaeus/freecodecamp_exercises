bd61eb378f6afcad0e2e42e42e2ae367f190f6e7
Uthaeus <romanlavery@gmail.com>
2020-08-14 10:13:56 -0600

    update

---
+++ scratchpaper.js
@@ -0,0 +1,28 @@
+
+function sym(...args) {
+  let possibles = [];
+  let final = [];
+  let marker;
+
+  for (let x = 0; x < args.length; x++) {
+    for (let y = 0; y < args[x].length; y++) {
+      marker = true;
+
+      for (let z = 0; z < args.length; z++) {
+        if (args[z].includes(args[x][y]) && z !== x) {
+          console.log('false', args[x][y]);
+          let marker = false;
+          console.log(marker);
+        }
+      }
+      console.log(marker);
+      if (marker == true) {
+        possibles.push(args[x][y]);
+      }
+    }
+  }
+
+  console.log(possibles)
+}
+
+sym([1, 2, 3], [5, 2, 1, 4]); // [3, 4, 5]
