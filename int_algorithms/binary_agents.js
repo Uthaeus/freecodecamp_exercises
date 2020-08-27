3d2cbbc12f949b3ff819cb7e5ecbae6763ccee47
Uthaeus <romanlavery@gmail.com>
2020-08-26 21:17:27 -0600

    Return an English translated sentence of the passed binary string

--- int_algorithms/binary_agents.js
+++ int_algorithms/binary_agents.js
@@ -5,17 +5,17 @@
 
 
 function binaryAgent(str) {
-  // let numArr = str.split(' ');
-  // let result = [];
+  let numArr = str.split(' ');
+  let result = [];
 
-  // for (var i = 0; i < str.length; i++) {
-  //   result.push(String.fromCharCode(parseInt(numArr[i], 2)));
-  // }
-  // return result.join('');
+  for (var i = 0; i < str.length; i++) {
+    result.push(String.fromCharCode(parseInt(numArr[i], 2)));
+  }
+  return result.join('');
 
-  return str.split(" ").map(function(elem) {
-    return String.fromCharCode(parseInt(elem, 2));
-  }).join("")
+  // return str.split(" ").map(function(elem) {
+  //   return String.fromCharCode(parseInt(elem, 2));
+  // }).join("")
 }
 
 console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
