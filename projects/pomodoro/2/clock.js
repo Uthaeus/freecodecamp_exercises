ee75cfc1475c7f55161e35ad7132c40baa7f6adf
Uthaeus <romanlavery@gmail.com>
2020-10-24 15:49:42 -0600

    completed logic for increment and decrement buttons

--- projects/pomodoro/2/clock.js
+++ projects/pomodoro/2/clock.js
@@ -1,6 +1,4 @@
-let mainDisplay = document.getElementById('main-display');
-let breakDisplay = document.getElementById('break-display');
-let timeDisplay = document.getElementById('time-display');
+
 
 const breakIncrement = () => {
     let currentValue = +document.getElementById('break-display').innerHTML
@@ -10,7 +8,22 @@ const breakIncrement = () => {
 }
 
 const breakDecrement = () => {
-    let currentDiv = document.getElementById('break-decrement');
-    let currentValue = +currentDiv.innerHTML;
-    console.log(currentValue);
+    let currentValue = +document.getElementById('break-display').innerHTML;
+    if (currentValue > 1) {
+        document.getElementById('break-display').innerHTML = currentValue - 1;
+    }
+}
+
+const timeIncrement = () => {
+    let currentValue = +document.getElementById('time-display').innerHTML;
+    if (currentValue < 45) {
+        document.getElementById('time-display').innerHTML = currentValue + 1;
+    }
+}
+
+const timeDecrement = () => {
+    let currentValue = +document.getElementById('time-display').innerHTML;
+    if (currentValue > 1) {
+        document.getElementById('time-display').innerHTML = currentValue - 1;
+    }
 }
