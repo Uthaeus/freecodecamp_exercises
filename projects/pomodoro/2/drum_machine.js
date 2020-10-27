1978ce1f1671795d5d969617853cd3cff61e3ef7
Uthaeus <romanlavery@gmail.com>
2020-10-27 09:29:44 -0600

    implemented keystroke listener, got rid of input text area

--- projects/pomodoro/2/drum_machine.js
+++ projects/pomodoro/2/drum_machine.js
@@ -1,12 +1,27 @@
 let source = document.getElementById('out');
 let result = document.getElementById('display');
 
+document.addEventListener("keypress", function onEvent(e) {
+    console.log(e.key);
+    myfunc(e.key)
+});
+
 const myfunc = (a) => {
-    console.log('func', a);
-    document.getElementById('out').value =  '';
-    document.getElementById('display').innerHTML = a;
+    console.log(a);
+    let drums = 'qweasdzxc';
+    //document.getElementById('out').value =  '';
+    if (drums.includes(a)) {
+        document.getElementById('display').innerHTML = a.toUpperCase();
+        // drum a .play()
+    }
 }
 
 const inputHandler = e => {
     result.innerHTML = e.target.value;
+}
+
+const drumClick = id => {
+    let drum = id[id.length - 1];
+    document.getElementById('display').innerHTML = drum;
+    console.log(drum);
 }
