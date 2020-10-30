b7cf4f3df443953a6977b4ded8a2a24f4e773b63
Uthaeus <romanlavery@gmail.com>
2020-10-30 09:28:53 -0600

    session buttons functional

--- projects/pomodoro/3/clock.js
+++ projects/pomodoro/3/clock.js
@@ -12,3 +12,17 @@ const breakDecrement = () => {
         currentVal.innerHTML = +currentVal.innerHTML - 1;
     }
 }
+
+const sessionIncrement = () => {
+    let currentVal = document.getElementById('session-length');
+    if (+currentVal.innerHTML < 60) {
+        currentVal.innerHTML = +currentVal.innerHTML + 1;
+    }
+}
+
+const sessionDecrement = () => {
+    let currentVal = document.getElementById('session-length')
+    if (+currentVal.innerHTML > 1) {
+        currentVal.innerHTML = +currentVal.innerHTML - 1;
+    }
+}
