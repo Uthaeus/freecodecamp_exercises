3ad097d5bf234f0d17ded90e85f89b8f73cd5e60
Uthaeus <romanlavery@gmail.com>
2020-10-30 10:10:50 -0600

    timer functional

--- projects/pomodoro/3/clock.js
+++ projects/pomodoro/3/clock.js
@@ -6,17 +6,29 @@ const startStop = () => {
     isRunning = !isRunning;
     if (isRunning) {
         myVar = setInterval(countdown, 1000);
+        document.getElementById('start_stop').innerHTML = 'pause';
     } else {
         clearInterval(myVar);
+        document.getElementById('start_stop').innerHTML = 'play';
     }
 }
 
 const countdown = () => {
     let currentVal = document.getElementById('time-left').innerHTML;
     let [min, sec] = currentVal.split(':');
-    console.log(min);
 
+    if (+sec - 1 < 0 && +min > 0) {
+        sec = 59;
+        min--;
+    } else if (+sec - 1 == 0 && +min == 0) {
+        intermission()
+    } else {
+        +sec--;
+    }
+    if (+min < 10) min = '0' + min.toString();
+    if (+sec < 10) sec = '0' + sec.toString();
 
+    document.getElementById('time-left').innerHTML = `${min}:${sec}`;
 }
 
 const breakIncrement = () => {
@@ -51,6 +63,7 @@ const sessionDecrement = () => {
 
 const reSet = () => {
     clearInterval(myVar);
+    document.getElementById('start_stop').innerHTML = 'play';
     document.getElementById('break-length').innerHTML = 5;
     document.getElementById('session-length').innerHTML = 25;
     document.getElementById('time-left').innerHTML = '25:00';
