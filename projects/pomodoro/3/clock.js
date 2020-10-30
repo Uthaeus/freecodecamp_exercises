705e57814d9d38b60fd0a5effa8a710a000b61f6
Uthaeus <romanlavery@gmail.com>
2020-10-30 09:51:51 -0600

    start stop button

--- projects/pomodoro/3/clock.js
+++ projects/pomodoro/3/clock.js
@@ -1,4 +1,24 @@
 
+let isRunning = false;
+
+const startStop = () => {
+    isRunning = !isRunning;
+    let x;
+
+    if (isRunning) {
+        x = setInterval(countdown(), 1000);
+    } else {
+        clearInterval(x);
+    }
+}
+
+const countdown = () => {
+    let currentVal = document.getElementById('time-left').innerHTML;
+    let [min, sec] = currentVal.split(':');
+
+    
+}
+
 const breakIncrement = () => {
     let currentVal = document.getElementById('break-length');
     if (+currentVal.innerHTML < 60) {
@@ -17,6 +37,7 @@ const sessionIncrement = () => {
     let currentVal = document.getElementById('session-length');
     if (+currentVal.innerHTML < 60) {
         currentVal.innerHTML = +currentVal.innerHTML + 1;
+        document.getElementById('time-left').innerHTML = `${currentVal.innerHTML}:00`;
     }
 }
 
@@ -24,5 +45,13 @@ const sessionDecrement = () => {
     let currentVal = document.getElementById('session-length')
     if (+currentVal.innerHTML > 1) {
         currentVal.innerHTML = +currentVal.innerHTML - 1;
+        document.getElementById('time-left').innerHTML = `${currentVal.innerHTML}:00`;
     }
+}
+
+const reSet = () => {
+    // clearInterval
+    document.getElementById('break-length').innerHTML = 5;
+    document.getElementById('session-length').innerHTML = 25;
+    document.getElementById('time-left').innerHTML = '25:00';
 }
